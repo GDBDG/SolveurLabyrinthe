366fc6f2717ddd196ba14d44295ce5dfb961a647
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2021-09-21 09:22:57 +0200

    Ajout Labyrinthe et cases

---
+++ GenerateurLabyrinthe/Case.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import structlog as structlog
+
+import GenerateurLabyrinthe.Labyrinthe as laby
+logger =structlog.getLogger(__name__)
+class Case:
+    """
+    Une case contient un booléen pour chacun de ses 4 murs
+    Les coordonées vont de 0 au nombre de case sur la ligne ou colonne du labyrinthe
+    Les coordonnées désignent le point en bas à gauche de la case
+    """
+
+    def __init__(self,labyrinthe: laby.Labyrinthe, abscisse=0, ordonnee=0, murHaut=True, murGauche=True, murDroit=True, murBas=True):
+        """
+        Constructeur d'une case
+        :param abscisse: coordonée de l'abscisse du mur
+        :param ordonnee: coordonée de l'ordonnée du mur
+        :param murHaut: booléen indiquant si la case a un mur vers le haut (nord)
+        :param murGauche: booléen indiquant si la case a un mur vers la gauche (ouest)
+        :param murDroit: booléen indiquant si la case a un mur vers la droite (est)
+        :param murBas: booléen indiquant si la case a un mur vers le bas (sud)
+        """
+        self.labyrinthe = labyrinthe
+        self.abscisse = abscisse
+        self.ordonnee = ordonnee
+        self.murHaut = murHaut
+        self.murBas = murBas
+        self.murGauche = murGauche
+        self.murDroit = murDroit
+        self.exploree = False
+
+    def supprimerMurHaut(self):
+        """
+        Permet de suppprimer le mur du haut, utile dans la génération de labyrinthe
+        Doit vérifier que ce n'est pas une case de la dernière ligne du labyrinthe (il suffit de regarder s'il existe
+        une case au dessus)
+        Modifie aussi la case du dessus
+        :return: None
+        """
+        assert (self.abscisse, self.ordonnee+1) in self.labyrinthe.cases.keys()
+        self.murHaut = False
+        self.labyrinthe.cases[(self.abscisse, self.ordonnee+1)].murBas = False
+
+
+    def supprimerMurBas(self):
+        """
+        Permet de suppprimer le mur du bas, utile dans la génération de labyrinthe
+        Doit vérifier que ce n'est pas une case de la première ligne du labyrinthe (il suffit de regarder s'il existe
+        une case au dessous)
+        :return: None
+        """
+        assert (self.abscisse, self.ordonnee-1) in self.labyrinthe.cases.keys()
+        self.murBas = False
+        self.labyrinthe.cases[(self.abscisse, self.ordonnee-1)].murHaut = False
+
+
+    def supprimerMurGauche(self):
+        """
+        Permet de suppprimer le mur de gauche, utile dans la génération de labyrinthe
+        Doit vérifier que ce n'est pas une case de la première colonne du labyrinthe (il suffit de regarder s'il existe
+        une case à gauche)
+        :return: None
+        """
+        assert (self.abscisse-1, self.ordonnee) in self.labyrinthe.cases.keys()
+        self.murGauche = False
+        self.labyrinthe.cases[(self.abscisse - 1, self.ordonnee)].murDroit = False
+
+
+    def supprimerMurDroit(self):
+        """
+        Permet de suppprimer le mur de droite, utile dans la génération de labyrinthe
+        Doit vérifier que ce n'est pas une case de la première colonne du labyrinthe (il suffit de regarder s'il existe
+        une case à droite)
+        :return: None
+        """
+        assert (self.abscisse+1, self.ordonnee) in self.labyrinthe.cases.keys()
+        self.murDroit = False
+        self.labyrinthe.cases[(self.abscisse + 1, self.ordonnee)].murGauche = False
+
+    def ploterCase(self, opti = True):
+        """
+        Trace la case
+        opti : mode optimisé, ne trace que les lignes du haut et celles de gauche, l'idée étant que les autres lignes
+        seront tracées par les cases de droite et celles du dessous.
+        :return:
+        """
+        if self.murHaut:
+            plt.plot([self.abscisse, self.abscisse+1], [self.ordonnee + 1, self.ordonnee+1], color="black")
+        if self.murGauche:
+            plt.plot([self.abscisse, self.abscisse], [self.ordonnee, self.ordonnee+1], color = "black")
+        if not opti:
+            if self.murBas:
+                plt.plot([self.abscisse, self.abscisse + 1], [self.ordonnee, self.ordonnee], color="black")
+            if self.murDroit:
+                plt.plot([self.abscisse + 1, self.abscisse + 1], [self.ordonnee, self.ordonnee + 1], color="black")
+
---
+++ GenerateurLabyrinthe/Labyrinthe.py
@@ -0,0 +1,39 @@
+from itertools import product
+
+CST_CROIX = 0.3
+import matplotlib.pyplot as plt
+
+
+class Labyrinthe:
+    def __init__(self, nbLigne, nbColonne):
+        import GenerateurLabyrinthe.Case as Case
+        """
+        Crée une grille pleine
+        """
+        self.nbLigne = nbLigne
+        self.nbColonne = nbColonne
+        self.cases = {(abscisse, ordonnee): Case.Case(self, abscisse, ordonnee) for abscisse, ordonnee in
+                      product(range(nbLigne), range(nbColonne))}
+
+    def plot_labyrinthe(self):
+        """
+        Trace le labyrinthe via matplotlib
+        :return:
+        """
+        # Tracer les cases
+        for case in self.cases.values():
+            case.ploterCase()
+        # Ajout de la ligne du bas et de la colonne de droite
+        plt.plot([0, self.nbLigne], [0, 0], color="black")
+        plt.plot([self.nbLigne, self.nbLigne], [0, self.nbColonne], color="black")
+        # Affichage de l'entrée (Croix noire)
+        plt.plot([0 + CST_CROIX, 1 - CST_CROIX], [0 + CST_CROIX, 1 - CST_CROIX], color="black")
+        plt.plot([0 + CST_CROIX, 1 - CST_CROIX], [1 - CST_CROIX, 0 + CST_CROIX], color="black")
+        plt.plot([self.nbLigne - 1 + CST_CROIX, self.nbLigne - CST_CROIX], [self.nbColonne - 1 + CST_CROIX, self.nbColonne - CST_CROIX], color="red")
+        plt.plot([self.nbLigne - 1 + CST_CROIX, self.nbLigne - CST_CROIX], [self.nbColonne  - CST_CROIX, self.nbColonne -1 + CST_CROIX], color="red")
+        plt.show()
+
+
+if __name__ == '__main__':
+    laby = Labyrinthe(10, 10)
+    laby.plot_labyrinthe()
